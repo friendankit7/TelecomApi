05ef7725c1837e03ae2fa82281d62833c39bb227
cduser <cduser123>
2018-11-14 20:01:45 +0530

    First Test Case added

--- telecomprovider/src/test/java/com/and/digital/api/telecomprovider/TelecomproviderApplicationTests.java
+++ telecomprovider/src/test/java/com/and/digital/api/telecomprovider/TelecomproviderApplicationTests.java
@@ -1,9 +1,15 @@
 package com.and.digital.api.telecomprovider;
 
+import java.util.List;
+
+import javax.validation.constraints.AssertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.and.digital.api.telecomprovider.controller.CustomerPhoneController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +19,11 @@ public class TelecomproviderApplicationTests {
 	public void contextLoads() {
 	}
 
+	
+	@Test
+	public void testAllPhoneNumberNotNull(){
+		CustomerPhoneController controller = new CustomerPhoneController();
+		List<PhoneNumberDTO> phoneNumberList =  controller.getAllPhoneNumbers();
+		assertTrue(phoneNumberList!=null);
+	}
 }
