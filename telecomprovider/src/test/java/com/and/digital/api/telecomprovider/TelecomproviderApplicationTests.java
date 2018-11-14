68cc1777c4cac8bab63617616d84728b14b4c135
cduser <cduser123>
2018-11-14 20:34:59 +0530

    Mockito added for test case

--- telecomprovider/src/test/java/com/and/digital/api/telecomprovider/TelecomproviderApplicationTests.java
+++ telecomprovider/src/test/java/com/and/digital/api/telecomprovider/TelecomproviderApplicationTests.java
@@ -1,57 +1,67 @@
 package com.and.digital.api.telecomprovider;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.and.digital.api.telecomprovider.controller.CustomerPhoneController;
 import com.and.digital.api.telecomprovider.dto.PhoneNumberDTO;
+import com.and.digital.api.telecomprovider.service.PhoneNumberService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TelecomproviderApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@InjectMocks
+	CustomerPhoneController controller;
+	
+	@Mock
+	PhoneNumberService phoneNumberService;
+	
+	@Before
+	public void init() {
+		controller = new CustomerPhoneController(phoneNumberService);
+		when(phoneNumberService.getAllPhoneNumbers()).thenReturn(new ArrayList<PhoneNumberDTO>());
 	}
 
 	
 	@Test
 	public void testAllPhoneNumberNotNull(){
-		CustomerPhoneController controller = new CustomerPhoneController();
 		List<PhoneNumberDTO> phoneNumberList =  controller.getAllPhoneNumbers();
 		assertTrue(phoneNumberList != null);
 	}
 	
 	@Test
 	public void testAllPhoneNumberNonEmptyList(){
-		CustomerPhoneController controller = new CustomerPhoneController();
 		List<PhoneNumberDTO> phoneNumberList =  controller.getAllPhoneNumbers();
 		assertTrue(!phoneNumberList.isEmpty());
 	}
 	
 	@Test
 	public void testCustomerPhoneNumberNotEmpty(){
-		CustomerPhoneController controller = new CustomerPhoneController();
 		List<PhoneNumberDTO> phoneNumberList =  controller.getCustomerPhoneNumbers(100);
 		assertTrue(!phoneNumberList.isEmpty());
 	}
 	
 	@Test
 	public void testCustomerPhoneNumberIsEmpty(){
-		CustomerPhoneController controller = new CustomerPhoneController();
 		List<PhoneNumberDTO> phoneNumberList =  controller.getCustomerPhoneNumbers(101);
 		assertTrue(phoneNumberList.isEmpty());
 	}
 	
 	@Test
 	public void testActivatePhoneNumberSuccess(){
-		CustomerPhoneController controller = new CustomerPhoneController();
 		PhoneNumberDTO phoneNumber =  controller.activatePhoneNumber("+2132090090");
 		assertEquals("Active", phoneNumber.getStatus());
 	}
