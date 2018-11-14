9187f863dfa634d585d6e7e40d0d893e02297184
cduser <cduser123>
2018-11-14 20:44:15 +0530

    Post mapping changed

--- telecomprovider/src/main/java/com/and/digital/api/telecomprovider/controller/CustomerPhoneController.java
+++ telecomprovider/src/main/java/com/and/digital/api/telecomprovider/controller/CustomerPhoneController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,8 +34,8 @@ public class CustomerPhoneController {
 		return phoneNumberService.getCustomerPhoneNumbers(customerId);
 	}
 
-	@PostMapping("/activatePhoneNumber/{phoneNumber}")
-	public PhoneNumberDTO activatePhoneNumber(@PathVariable String phoneNumber) {
+	@PostMapping("/activatePhoneNumber/")
+	public PhoneNumberDTO activatePhoneNumber(@RequestBody String phoneNumber) {
 		return phoneNumberService.activatePhoneNumber(phoneNumber);
 	}
 }
